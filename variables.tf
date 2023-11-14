fe756a4356999cf7117e78e1e50a2982ecd73edd
Ravikumar <k.ravikumarnaidu59@gmail.com>
2023-11-14 20:02:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "ami" {
    type        = string
    description = "Ubuntu AMI ID in N. Virginia Region"
-   default     = "ami-065deacbcaac64cf2"
+   default     = "ami-06aa3f7caf3a30282"
 }
 
 variable "instance_type" {
